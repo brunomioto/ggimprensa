9eef4b752d772ff43a8376b1a117c6100a4245ad
brunomioto <brunomioto97@gmail.com>
2022-05-24 20:35:31 -0300

    v1

---
+++ R/tema_poder360.R
@@ -0,0 +1,29 @@
+#' Tema inspirado nos gráficos do jornal Poder 360
+#'
+#'
+#' @inheritParams ggplot2::theme_grey
+#' @family themes poder360
+#' @export
+#' @importFrom grid unit
+#'
+tema_poder360 <- function(base_size = 12, base_family = "sans") {
+  (theme_foundation(base_size = base_size, base_family = base_family)
+   + theme(plot.margin = margin(t = 18, r = 20, b = 6, l = 20),
+         plot.background = element_rect(fill = "#eceff1", color = NA),
+         panel.background = element_rect(fill = NA, color = NA),
+         plot.title = element_text(hjust = 0.5,
+                                   color = "#0a4b7f",
+                                   face = "bold",
+                                   family = "Arial",
+                                   size = rel(2)),
+         plot.subtitle = element_text(hjust = 0.5,
+                                      color = "#263238"),
+         plot.caption = element_text(hjust = 0,
+                                     color = "#78919d"),
+         axis.text.x = element_text(color = "#263238"),
+         axis.text.y = element_text(color = "#78909c"),
+         axis.title = element_blank(),
+         axis.ticks = element_blank(),
+         panel.grid = element_blank()
+   ))
+}
---
+++ R/theme_foundation.R
@@ -0,0 +1,36 @@
+#' Foundation Theme
+#'
+#' This theme is designed to be a foundation from which to build new
+#' themes, and not meant to be used directly. \code{theme_foundation()}
+#' is a complete theme with only minimal number of elements defined.
+#' It is easier to create new themes by extending this one rather
+#' than \code{\link[ggplot2]{theme_gray}()} or \code{\link[ggplot2]{theme_bw}()},
+#' because those themes define elements deep in the hierarchy.
+#'
+#' This theme takes \code{\link[ggplot2]{theme_gray}()} and sets all
+#' \code{colour} and \code{fill} values to \code{NULL}, except for the top-level
+#' elements (\code{line}, \code{rect}, and \code{title}), which have
+#' \code{colour = "black"}, and \code{fill = "white"}. This leaves the spacing
+#' and-non colour defaults of the default \pkg{ggplot2} themes in place.
+#'
+#' @inheritParams ggplot2::theme_grey
+#'
+#' @family themes
+#' @export
+#' @importFrom ggplot2 theme_grey
+theme_foundation <- function(base_size=12, base_family="") {
+  thm <- theme_grey(base_size = base_size, base_family = base_family)
+  for (i in names(thm)) {
+    if ("colour" %in% names(thm[[i]])) {
+      thm[[i]]["color"] <- list(NULL)
+    }
+    if ("fill" %in% names(thm[[i]])) {
+      thm[[i]]["fill"] <- list(NULL)
+    }
+  }
+  thm + theme(panel.border = element_rect(fill = NA),
+              legend.background = element_rect(color = NA),
+              line = element_line(color = "black"),
+              rect = element_rect(fill = "white", color = NA),
+              text = element_text(color = "black"))
+}
