556b6d173d0a041c2300f33a20fa0682eeb7594e
brunomioto <brunomioto97@gmail.com>
2022-06-04 15:13:08 -0300

    ggimprensa internal

--- R/ggimprensa-package.R
+++ R/ggimprensa-package.R
@@ -7,4 +7,5 @@
 #' @importFrom ggplot2 ggplot rel theme margin element_rect element_line element_text element_blank ggproto aes
 #' @importFrom graphics abline axis text points
 #' @keywords internal
+#' @noRd
 NULL
