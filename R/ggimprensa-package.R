6d676e62697571af350b05b536dc9cb7e9899a1b
brunomioto <brunomioto97@gmail.com>
2022-06-04 11:38:49 -0300

    internal function

--- R/ggimprensa-package.R
+++ R/ggimprensa-package.R
@@ -6,4 +6,5 @@
 #' @docType package
 #' @importFrom ggplot2 ggplot rel theme margin element_rect element_line element_text element_blank ggproto aes
 #' @importFrom graphics abline axis text points
+#' @keywords internal
 NULL
